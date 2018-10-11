cb581e51eda8f51afc9cb2176140bbe8a3a2aef7
Maja Desic <desicmaja@yahoo.com>
2018-10-11 10:46:38 +0200

    WIP racunanje sati

--- main.js
+++ main.js
@@ -13,13 +13,28 @@ const pasus1= document.getElementById("pera")
 function izracunajDane() {
     var satiEkspert=10000;
     var satiPocetnik=2000;
-    var brojSatiDnevno=document.getElementById("unos").value;
-    var brojDana = satiPocetnik / brojSatiDnevno;
-    console.log(brojDana);
-    console.log("Broj dana ucenja do pocetnika je:",brojDana);
-    var brojGodinaDoPocetnika=brojDana / 365;
+
+    var brojSatiDnevnoPocetnik=document.getElementById("unos").value;
+    var brojDanaPocetnik = satiPocetnik / brojSatiDnevnoPocetnik;
+    console.log(brojDanaPocetnik);
+    console.log("Broj dana ucenja do pocetnika je:",brojDanaPocetnik);
+    var brojGodinaDoPocetnika=brojDanaPocetnik / 365;
     console.log("Broj godina ucenja do pocetnika je:",brojGodinaDoPocetnika.toFixed(2));
-    pasus1.innerText = 'Broj dana ucenja do pocetnika je: ' + brojDana;
+    pasus1.innerText = 'Broj dana ucenja do pocetnika je: ' + brojDanaPocetnik;
+    brojSatiPraksa = 8;
+    var brojDanaPraksa = (satiEkspert - satiPocetnik)/brojSatiPraksa;
+    var brojGodinaPraksi = brojDanaPraksa / 365;
+    console.log("Broj godina na praksi:",brojGodinaPraksi.toFixed(2));
+    var brojGodinaPraksiGod = brojGodinaPraksi.toFixed(0);
+    var brojMeseciNaPraksiX = (brojGodinaPraksi % 365)/30;
+    var brojMeseciNaPraksi = brojMeseciNaPraksiX.toFixed(0);
+    var brojdanaNaPraksiX = brojMeseciNaPraksi % 30;
+    var brojdanaNaPraksi = brojdanaNaPraksiX.toFixed(0);
+
+    console.log("Broj godina na praksi:",brojGodinaPraksiGod,"godina",brojMeseciNaPraksi,"meseci",brojdanaNaPraksi,"dana");
+
+
+
 }
 
 const dugme = document.getElementById("klikni")
